b61aee288cf5cd9113d248e8894320d100f804cd
dhirajkarangale <dakarangale02@gmail.com>
2023-02-25 04:42:11 +0530

    Search Completed

--- PocketMoney/src/pages/findJob/index.js
+++ PocketMoney/src/pages/findJob/index.js
@@ -1,74 +1,105 @@
-import React, { useState,useEffect } from "react";
-import { Card, Button, Container, Row, Col } from "react-bootstrap";
+import React, { useState, useEffect } from "react";
+import { Card, Button, Container, Row, Col, Form, FormControl } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import officeImg from '../../assets/profile/office.jpeg'
 
 import { projectFirestore } from "../../firebase/config";
-import { collection,getDocs } from 'firebase/firestore/lite';
+import { collection, getDocs } from 'firebase/firestore/lite';
 
 function FindJob() {
 
-  const [jobs,setJobs]=useState(null)
-  const [post,setPost]=useState(null)
-  console.log(jobs)
+  const [jobs, setJobs] = useState(null)
+  const [post, setPost] = useState(null)
+  const [searchTerm, setSearchTerm] = useState('');
 
-  useEffect(()=>{
-    const colRef=collection(projectFirestore,'jobs')
-    getDocs(colRef).then((snapshot)=>{
-      if(snapshot.empty)
-      {
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+
+    const colRef = collection(projectFirestore, 'jobs')
+    getDocs(colRef).then((snapshot) => {
+      if (snapshot.empty) {
         console.log("No jobs to load")
       }
-      else{
-        let result=[]
-        snapshot.docs.forEach(doc=>{
-          result.push({id:doc.id,...doc.data()})
+      else {
+        let result = []
+        snapshot.docs.forEach(doc => {
+
+          if (doc.data().desc.toLocaleLowerCase().includes(searchTerm.toLocaleLowerCase()) || doc.data().profile.toLocaleLowerCase().includes(searchTerm.toLocaleLowerCase())) {
+            result.push({ id: doc.id, ...doc.data() })
+          }
         })
         setJobs(result);
       }
-    }).catch(err=>{
+    }).catch(err => {
       console.log(err)
     })
-  },[])
-  useEffect(()=>{
-    const colRef=collection(projectFirestore,'post')
-    getDocs(colRef).then((snapshot)=>{
-      if(snapshot.empty)
-      {
+  }
+
+  const handleChange = (event) => {
+    setSearchTerm(event.target.value);
+  }
+
+
+  useEffect(() => {
+    const colRef = collection(projectFirestore, 'jobs')
+    getDocs(colRef).then((snapshot) => {
+      if (snapshot.empty) {
+        console.log("No jobs to load")
+      }
+      else {
+        let result = []
+        snapshot.docs.forEach(doc => {
+          result.push({ id: doc.id, ...doc.data() })
+        })
+        setJobs(result);
+      }
+    }).catch(err => {
+      console.log(err)
+    })
+  }, [])
+
+  useEffect(() => {
+    const colRef = collection(projectFirestore, 'post')
+    getDocs(colRef).then((snapshot) => {
+      if (snapshot.empty) {
         console.log("No post to load")
       }
-      else{
-        let result=[]
-        snapshot.docs.forEach(doc=>{
-          result.push({id:doc.id,...doc.data()})
+      else {
+        let result = []
+        snapshot.docs.forEach(doc => {
+          result.push({ id: doc.id, ...doc.data() })
         })
         setPost(result);
       }
-    }).catch(err=>{
+    }).catch(err => {
       console.log(err)
     })
-  },[])
+  }, [])
+
 
 
 
   return (
     <Container className="mt-4">
-      <div className="d-flex justify-content-end mb-4">
-        <input
-          type="text"
-          placeholder="Search"
-          // value={searchQuery}
-          // onChange={handleSearch}
-          style={{ marginRight: "10px" }}
-        />
+
+      <div className="position-relative position-relative-example">
+        <div className="position-absolute bottom-40 end-0">
+          <Form onSubmit={handleSubmit}>
+            <FormControl type="text" placeholder="Search" className="mr-sm-2" onChange={handleChange} value={searchTerm} />
+            <Button variant="outline-success" type="submit">Search</Button>
+          </Form>
+        </div>
       </div>
+
       <h2 className="text-center">Job Openings</h2><br />
       <Link to={`/user/setRole`}>
-      <div className="d-flex align-items-center mb-4 text-center">
-        <Button variant="dark" size="lg">
-           Post
-        </Button>
-      </div>
+        <div className="d-flex align-items-center mb-4 text-center">
+          <Button variant="dark" size="lg">
+            Post
+          </Button>
+        </div>
       </Link>
       {jobs && jobs.map((job, id) => {
         return (
@@ -87,19 +118,19 @@ function FindJob() {
                     {job.profile}, {job.company}
                   </Card.Title>
                   <Card.Text>
-                    {job.desc.substring(0,100)}....<br />
+                    {job.desc.substring(0, 100)}....<br />
                   </Card.Text>
                   <Card.Text>
                     Location: {job.location}<br />
                   </Card.Text>
                   <Link to={`/jobform/${job.id}`}>
-                    <Button  variant="dark"style={{backgroundColor:"green"}} >Apply Now</Button>
+                    <Button variant="dark" style={{ backgroundColor: "green" }} >Apply Now</Button>
                   </Link>
-                  
+
                   <Link to={`/findJob/${job.id}`}>
-                  <Button variant="dark" >Read more</Button>
+                    <Button variant="dark" >Read more</Button>
                   </Link>
-                  
+
                 </Card.Body>
               </Col>
             </Row>
@@ -123,13 +154,13 @@ function FindJob() {
                     {post.name}, {post.role}
                   </Card.Title>
                   <Card.Text>
-                    {post.desc.substring(0,100)}<br />
+                    {post.desc.substring(0, 100)}<br />
                   </Card.Text>
                   <Link to={`/hireme/${post.id}`}>
-                    <Button variant="dark" style={{backgroundColor:"blue"}}>Hire Me !</Button>
+                    <Button variant="dark" style={{ backgroundColor: "blue" }}>Hire Me !</Button>
                   </Link>
                   <Link to={`/findJob/post/${post.id}`}>
-                  <Button variant="dark" >Read more</Button>
+                    <Button variant="dark" >Read more</Button>
                   </Link>
                 </Card.Body>
               </Col>
@@ -137,7 +168,6 @@ function FindJob() {
           </Card>
         );
       })}
-
     </Container>
   );
 }
@@ -146,116 +176,36 @@ export default FindJob;
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import React, { useState, useEffect } from "react";
-// import { Card, Button, Container, Row, Col, Form, FormControl } from "react-bootstrap";
-// import { Link } from "react-router-dom";
-// import officeImg from '../../assets/profile/office.jpeg'
-
-// import { projectFirestore } from "../../firebase/config";
-// import { collection, getDocs } from 'firebase/firestore/lite';
-
-// function FindJob() {
-
-//   const [jobs, setJobs] = useState(null)
-//   const [post, setPost] = useState(null)
-//   const [searchTerm, setSearchTerm] = useState('');
-
-//   const handleSubmit = (event) => {
-//     event.preventDefault();
-//     setJobs(jobs.filter((job) =>
-//     job.pr.toLocaleLowerCase().includes(searchTerm.toLocaleLowerCase())
-//     ));
-//     console.log("==========================> DK Searching item : " + searchTerm); // replace with your own logic
-//   }
-
-//   const handleChange = (event) => {
-//     setSearchTerm(event.target.value);
-//   }
-
-//   useEffect(() => {
-//     const colRef = collection(projectFirestore, 'jobs')
-//     getDocs(colRef).then((snapshot) => {
-//       if (snapshot.empty) {
-//         console.log("No jobs to load")
-//       }
-//       else {
-//         let result = []
-//         snapshot.docs.forEach(doc => {
-//           result.push({ id: doc.id, ...doc.data() })
-//         })
-//         setJobs(result);
-//       }
-//     }).catch(err => {
-//       console.log(err)
-//     })
-//   }, [])
-
-//   useEffect(() => {
-//     const colRef = collection(projectFirestore, 'post')
-//     getDocs(colRef).then((snapshot) => {
-//       if (snapshot.empty) {
-//         console.log("No post to load")
-//       }
-//       else {
-//         let result = []
-//         snapshot.docs.forEach(doc => {
-//           result.push({ id: doc.id, ...doc.data() })
-//         })
-//         setPost(result);
-//       }
-//     }).catch(err => {
-//       console.log(err)
-//     })
-//   }, [])
-
-
 //   return (
 //     <Container className="mt-4">
 
-//       <Form inline onSubmit={handleSubmit}>
-//         <FormControl type="text" placeholder="Search" className="mr-sm-2" onChange={handleChange} value={searchTerm} />
-//         <Button variant="outline-success" type="submit">Search</Button>
-//       </Form>
-
-
-
+//       <div className="position-relative position-relative-example">
+//         <div className="position-absolute bottom-30 end-0">
+//           <Form onSubmit={handleSubmit}>
+//             <FormControl type="text" placeholder="Search" className="mr-sm-1" onChange={handleChange} value={searchTerm} />
+//             <Button variant="outline-success" type="submit">Search</Button>
+//           </Form>
+//         </div>
+//       </div>
 
 //       <h2 className="text-center">Job Openings</h2><br />
 //       <Link to={`/user/setRole`}>
+
+
+
 //         <div className="d-flex align-items-center mb-4 text-center">
+
 //           <Button variant="dark" size="lg">
 //             Post
 //           </Button>
+
+
+
 //         </div>
 //       </Link>
-
-
 //       {jobs && jobs.map((job, id) => {
-
 //         return (
-
-//           < Card style={{ marginBottom: "40px" }
-//           } key={id} >
+//           <Card style={{ marginBottom: "40px" }} key={id}>
 //             <Row>
 //               <Col md={3} className="text-center p-4">
 //                 <Card.Img
@@ -289,43 +239,39 @@ export default FindJob;
 //           </Card>
 //         );
 //       })}
-//       {
-//         post && post.map((post, id) => {
-//           return (
-//             <Card style={{ marginBottom: "40px" }} key={id}>
-//               <Row>
-//                 <Col md={3} className="text-center p-4">
-//                   <Card.Img
-//                     variant="top"
-//                     src={officeImg}
-//                     style={{ width: "100%" }}
-//                   />
-//                 </Col>
-//                 <Col>
-//                   <Card.Body>
-//                     <Card.Title>
-//                       {post.name}, {post.role}
-//                     </Card.Title>
-//                     <Card.Text>
-//                       {post.desc.substring(0, 100)}<br />
-//                     </Card.Text>
-//                     <Link to={`/hireme/${post.id}`}>
-//                       <Button variant="dark" style={{ backgroundColor: "blue" }}>Hire Me !</Button>
-//                     </Link>
-//                     <Link to={`/findJob/post/${post.id}`}>
-//                       <Button variant="dark" >Read more</Button>
-//                     </Link>
-//                   </Card.Body>
-//                 </Col>
-//               </Row>
-//             </Card>
+//       {post && post.map((post, id) => {
+//         return (
+//           <Card style={{ marginBottom: "40px" }} key={id}>
+//             <Row>
+//               <Col md={3} className="text-center p-4">
+//                 <Card.Img
+//                   variant="top"
+//                   src={officeImg}
+//                   style={{ width: "100%" }}
+//                 />
+//               </Col>
+//               <Col>
+//                 <Card.Body>
+//                   <Card.Title>
+//                     {post.name}, {post.role}
+//                   </Card.Title>
+//                   <Card.Text>
+//                     {post.desc.substring(0, 100)}<br />
+//                   </Card.Text>
+//                   <Link to={`/hireme/${post.id}`}>
+//                     <Button variant="dark" style={{ backgroundColor: "blue" }}>Hire Me !</Button>
+//                   </Link>
+//                   <Link to={`/findJob/post/${post.id}`}>
+//                     <Button variant="dark" >Read more</Button>
+//                   </Link>
+//                 </Card.Body>
+//               </Col>
+//             </Row>
+//           </Card>
+//         );
+//       })}
 
-
-//           );
-//         })
-//       }
-
-//     </Container >
+//     </Container>
 //   );
 // }
 
